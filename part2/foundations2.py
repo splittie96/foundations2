8d01272a3e6c4a20de5900e3785e61dcb6432248
splittie96 <robbie.gavin.henderson@gmail.com>
2013-02-22 17:19:29 +0000

    changed folder names to make sense

--- part2/foundations2.py
+++ part2/foundations2.py
@@ -1,59 +1,144 @@
+#CHARACTER ENCODING INFO
+#ALLOWS USE OF '#' WITHOUT ANGRY ERROR MESSAGES
 # coding: utf-8
+
+#IMPORT JSON LIBRARY
+import json
+#GLOBAL DICTIONARY OF ALL PARSED VARIABLES
+variable_dict={}
+
+"""===============================
+RECURSIVE PARSING OF OPERATORS
+==============================="""
+def deal_with_new_node(argument):
+    #takes in the list of arguments ascociated with the argument key
+    argv = argument["arguments"]
+    #checks
+    if argument["operator"] == "set":
+        newset = set([])
+        for x in argv:
+            if isinstance(x, int):
+                newset.add(x)
+            elif "operator" in x:
+		newset.add(deal_with_new_node(x))
+            elif "variable" in x:
+		newset.add(variable_dict[x["variable"]])
+            new_frozenset = frozenset(newset)
+        return new_frozenset
+        
+    elif argument["operator"] == "tuple":
+	tuple_contents = []
+	for x in argv:
+            if isinstance(x, int):
+		tuple_contents.append(x)
+            elif "operator" in x:
+		tuple_contents.append(deal_with_new_node(x))
+            elif "variable" in x:
+		tuple_contents.append(variable_dict[x["variable"]])
+	new_tuple = tuple(tuple_contents)
+	return new_tuple
     
-def output(t):
+    elif argument["operator"] == "equal":
+	comparisons = []
+        counter = 0
+	for x in argv:
+            if isinstance(x, int):
+		comparisons.append(x)
+            elif "operator" in x:
+		comparisons.append(deal_with_new_node(x))
+            elif "variable" in x:
+		comparisons.append(variable_dict[x["variable"]])
+            counter = counter + 1
+        if comparisons[0] == comparisons[1]:
+            if not isinstance(comparisons[1],int):
+                return 1
+        
+    elif argument["operator"] == "member":
+	comparisons = []
+        counter = 0
+	for x in argv:
+            if isinstance(x, int):
+		comparisons.append(x)
+            elif "operator" in x:
+		comparisons.append(deal_with_new_node(x))
+            elif "variable" in x:
+		comparisons.append(variable_dict[x["variable"]])
+            counter = counter + 1
+        if comparisons[0] in comparisons[1]:
+            return 1
+    
+    return 0
+
+
+"""===============================
+INITIALIZE PARSING OF JSON FILE
+==============================="""
+def parse_json(current):
+    if current["operator"]=="equal":
+        argv = current["arguments"]
+        var = argv[0]["variable"]
+        if isinstance(argv[1], int):
+            variable_dict[var] = argv[1]
+        else:
+            variable_dict[var] = deal_with_new_node(argv[1])
+    f.write (var + " = ")			
+    write_out(variable_dict[var])
+    f.write(';\n')
+
+
+"""====================================
+METHOD TO CARRY OUT RECURSIVE PRINTING
+===================================="""
+def output(out):
     counter = 0
-    for x in t:
+    for x in out:
         if isinstance(x,frozenset):
             if counter != 0:
-                print',',
-            print '{',
+                f.write(',')
+            f.write('{')
             output(x)
-            print'}',
+            f.write('}')
             counter = counter+1
         elif isinstance(x,tuple):
             if counter != 0:
-                print',',
-            print'(',
+                f.write(',')
+            f.write('(')
             output(x)
-            print')',
+            f.write (')')
             counter = counter+1
         else:
             if counter != 0:
-                print',',
-            print(x),
+                f.write(',')
+            f.write('%d' % x)
             counter = counter+1
-    
 
-x = []
-x.append(8)
-x.append(frozenset([1,2,3,4,5,6,7,x[0]]))
-x.append(frozenset([x[1],tuple([1,x[1]])]))
-x.append(tuple([x[2],x[1]]))
-x.append(frozenset([x[3]]).union(x[2]))
-x.append(x[4].difference(frozenset([x[1]])))
-x.append(x[4].intersection(frozenset([x[1]])))
 
-for current in x:
-    counter = 0
-    if isinstance(current,frozenset):
-        print '{',
-        if counter != 0:
-            print',',
-        output(current)
-        print '}',
-        counter = counter+1
-    elif isinstance(current,tuple):
-        print '(',
-        if counter != 0:
-            print',',
-        output(current)
-        print ')',
-        counter = counter+1
-    else:
-        print current,
-        counter = counter+1
-<<<<<<< HEAD
-    print
-=======
-    print
->>>>>>> afae181d4a32f0a2aaaa4db2558513587f02a3d2
+"""===============================
+METHOD TO INITIALIZE PRINTING
+==============================="""
+def write_out(to_write):
+    if isinstance(to_write, int):
+        f.write('%d' % to_write)
+    elif isinstance(to_write, frozenset):
+        f.write('{')
+        output(to_write)
+        f.write('}')
+    elif isinstance(to_write,tuple):
+        f.write('(')
+        output(to_write)
+        f.write (')')
+
+
+"""===============================
+MAIN PROGRAM RUNS HERE
+==============================="""
+f = open('output.txt', 'w')
+json_file = open('input.json', 'r')
+input_data = json.load(json_file)
+
+print 'output to \'output.txt\' '
+for x in input_data["statement-list"]:
+    parse_json(x)
+
+f.close()
+json_file.close()
